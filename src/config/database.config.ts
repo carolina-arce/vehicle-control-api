fccb87cf8abec144d62f1a0b614f9271f13607d5
carolina-arce <80922394+carolina-arce@users.noreply.github.com>
2026-04-28 21:40:05 -0300

    Update database.config.ts

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -1,5 +1,6 @@
 import { registerAs } from '@nestjs/config'
 
+
 export default registerAs('database', () => ({
   host:     process.env.DB_HOST     ?? 'localhost',
   port:     parseInt(process.env.DB_PORT ?? '5432'),
